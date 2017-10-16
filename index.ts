3d9c8cc9a6af51924b3e6c946b80cd904daa8c92
Jurgis Bridzius <jurgis@bridzius.lt>
2017-10-16 21:45:57 +0300

    [4] Removed deps and added lints

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { mkdirSync, appendFileSync, writeFileSync, readdirSync, rename, statSync, existsSync, readFileSync } from "fs";
+import { appendFileSync, existsSync, mkdirSync, readdirSync, readFileSync, rename, statSync, writeFileSync } from "fs";
 import { join } from "path";
 import { cwd } from "process";
 import * as website from "./website.json";
@@ -6,9 +6,11 @@ import * as website from "./website.json";
 function getIndexTemplate(data) {
   const templatePath = join(cwd(), "template.html");
   const templateExists = existsSync(templatePath);
-  if (!templateExists) throw Error("no template file");
+  if (!templateExists) {
+    throw Error("no template file");
+  }
   let template = readFileSync(templatePath).toString();
-  for (let opt in data) {
+  for (const opt of Object.keys(data)) {
     template = template.replace(`<//${opt.toUpperCase()}//>`, data[opt]);
   }
   return template.split("<//CONTENT//>");
@@ -19,25 +21,22 @@ function getFileContent(filePath) {
   return fileExists ? readFileSync(filePath).toString().split("\n") : "";
 }
 
-
 function getAllFileContent() {
   const textFiles = readdirSync(join(cwd(), "text"));
   return textFiles.reduce((output, file) => {
     return output.concat(getFileContent(join(cwd(), "text", file)));
-  }, [])
+  }, []);
 }
-
 
 function createWebsite(fileName) {
   const template = getIndexTemplate(website);
   const fileContent = getAllFileContent();
   const filePath = `.public/${fileName}.html`;
   writeFileSync(filePath, template[0]);
-  fileContent.forEach(line => {
+  fileContent.forEach((line) => {
     appendFileSync(filePath, `<p>${line}</p>`);
   });
   appendFileSync(filePath, template[1]);
 }
 
 createWebsite("index");
-
