234180af2222ac610faf655c0b3de5eca80e6c58
Jurgis Bridzius <jurgis@bridzius.lt>
2018-12-13 23:36:49 +0200

    Feat - Added basic logging for output

--- src/composer.ts
+++ src/composer.ts
@@ -1,94 +1,105 @@
-import {execSync} from "child_process";
+import { execSync } from "child_process";
 import {
-    appendFileSync,
-    existsSync,
-    mkdirSync,
-    readdirSync,
-    readFileSync,
-    rename,
-    statSync,
-    writeFileSync,
+  appendFileSync,
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  readFileSync,
+  statSync,
+  writeFileSync,
 } from "fs";
-import {
-    extname,
-    join,
-} from "path";
-import {
-    argv,
-    cwd,
-} from "process";
+import { extname, join } from "path";
+import { argv, cwd } from "process";
 import Config from "./config";
 
 function getConfigFile(args) {
-    const simplifiedArgs: string[] = args.slice(2);
-    if (typeof simplifiedArgs[0] === "string" &&
-        extname(simplifiedArgs[0]) === ".json" &&
-        existsSync(simplifiedArgs[0])) {
-        return simplifiedArgs[0];
-    } else {
-        throw Error("No input file specified");
-    }
+  const simplifiedArgs: string[] = args.slice(2);
+  if (
+    typeof simplifiedArgs[0] === "string" &&
+    extname(simplifiedArgs[0]) === ".json" &&
+    existsSync(simplifiedArgs[0])
+  ) {
+    return simplifiedArgs[0];
+  } else {
+    throw Error("No input file specified");
+  }
 }
 
 function getIndexTemplate(data) {
-    const templatePath = join(cwd(), "template.html");
-    const templateExists = existsSync(templatePath);
-    if (!templateExists) {
-        throw Error("no template file");
-    }
-    let template = readFileSync(templatePath).toString();
-    for (const opt of Object.keys(data)) {
-        template = template.replace(`<//${opt.toUpperCase()}//>`, data[opt]);
-    }
-    return template.split("<//CONTENT//>");
+  const templatePath = join(cwd(), "template.html");
+  const templateExists = existsSync(templatePath);
+  if (!templateExists) {
+    throw Error("no template file");
+  }
+  let template = readFileSync(templatePath).toString();
+  for (const opt of Object.keys(data)) {
+    template = template.replace(`<//${opt.toUpperCase()}//>`, data[opt]);
+  }
+  return template.split("<//CONTENT//>");
 }
 
 function getFileContent(filePath) {
-    const fileExists = existsSync(filePath);
-    return fileExists ? readFileSync(filePath).toString().split("\n") : "";
+  const fileExists = existsSync(filePath);
+  return fileExists
+    ? readFileSync(filePath)
+        .toString()
+        .split("\n")
+    : "";
 }
 
 function getFSDate(filePath) {
-    return statSync(filePath).mtime.getTime();
+  return statSync(filePath).mtime.getTime();
 }
 
 function getGitDate(filePath) {
-    const gitDate = execSync(`git log -1 --format="%at" -- ${filePath}`);
-    return parseInt(gitDate.toString(), 10);
+  const gitDate = execSync(`git log -1 --format="%at" -- ${filePath}`);
+  return parseInt(gitDate.toString(), 10);
 }
 
 function getAllFileContent(inputDir, inputType) {
-    const textFiles = readdirSync(join(cwd(), inputDir));
-    const parsedFiles = textFiles.map((file) => {
-        return {
-            name: file,
-            time: inputType === "git" ? getGitDate(join(inputDir, file)) : getFSDate(join(cwd(), inputDir, file)),
-          };
-    });
-    const sortedFiles = parsedFiles.sort((file1, file2) => file2.time - file1.time).map((file) => file.name);
-    return sortedFiles.reduce((output, file) => {
-        return output.concat(getFileContent(join(cwd(), inputDir, file)));
-    }, []);
+  const textFiles = readdirSync(join(cwd(), inputDir));
+  const parsedFiles = textFiles.map((file) => {
+    return {
+      name: file,
+      time:
+        inputType === "git"
+          ? getGitDate(join(inputDir, file))
+          : getFSDate(join(cwd(), inputDir, file)),
+    };
+  });
+  const sortedFiles = parsedFiles
+    .sort((file1, file2) => file2.time - file1.time)
+    .map((file) => file.name);
+  return sortedFiles.reduce((output, file) => {
+    return output.concat(getFileContent(join(cwd(), inputDir, file)));
+  }, []);
 }
 
 function createOutputFile(outputDir, filename) {
-    if (!existsSync(join(cwd(), outputDir))) {
-        mkdirSync(join(cwd(), outputDir));
-    }
-    return join(outputDir, `${filename}`);
+  if (!existsSync(join(cwd(), outputDir))) {
+    mkdirSync(join(cwd(), outputDir));
+  }
+  return join(outputDir, `${filename}`);
 }
 
 export default function createWebsite() {
-    const configFile = getConfigFile(argv);
-    const Configuration = new Config(configFile);
-    const OutputFile = createOutputFile(Configuration.output, Configuration.filename);
-    const template = getIndexTemplate(Configuration);
-    const fileContent = getAllFileContent(Configuration.input, Configuration.inputType);
-    writeFileSync(OutputFile, template[0]);
-    fileContent.forEach((line) => {
-        if (line !== "") {
-            appendFileSync(OutputFile, `<p>${line}</p>`);
-        }
-    });
-    appendFileSync(OutputFile, template[1]);
+  const configFile = getConfigFile(argv);
+  const Configuration = new Config(configFile);
+  const OutputFile = createOutputFile(
+    Configuration.output,
+    Configuration.filename,
+  );
+  const template = getIndexTemplate(Configuration);
+  const fileContent = getAllFileContent(
+    Configuration.input,
+    Configuration.inputType,
+  );
+  writeFileSync(OutputFile, template[0]);
+  fileContent.forEach((line) => {
+    if (line !== "") {
+      appendFileSync(OutputFile, `<p>${line}</p>\n`);
+    }
+  });
+  console.log(`Generating blankpage to ${OutputFile}`);
+  appendFileSync(OutputFile, template[1]);
 }
--- src/config.ts
+++ src/config.ts
@@ -1,49 +1,63 @@
-import {
-    existsSync,
-    readFileSync,
-} from "fs";
+import { existsSync, readFileSync } from "fs";
 import ConfigurationError from "./config-error";
 
-export default class BlankpageConfig {
-    public filename: any;
-    public title: string;
-    public input: string;
-    public inputType: string;
-    public output: string;
-    public header: string;
-    public subheader: string;
-    constructor(private confpath: any) {
-        const conf = getBlankConf(confpath);
-        validate(conf);
-        this.title = conf.title || "";
-        this.input = conf.input || "txt";
-        this.inputType = conf.inputType || "fs";
-        this.output = conf.output || "dist";
-        this.header = conf.header || "";
-        this.subheader = conf.subheader || "";
-        this.filename = conf.filename || "index.html";
-    }
+interface BlankConfig {
+  filename: any;
+  title: string;
+  input: string;
+  inputType: string;
+  output: string;
+  header: string;
+  subheader: string;
+}
+
+export default class BlankpageConfig implements BlankConfig {
+  public title: any;
+  public input: any;
+  public inputType: any;
+  public output: any;
+  public header: any;
+  public subheader: any;
+  public filename: any;
+  constructor(private confpath: any) {
+    const conf = getBlankConf(confpath);
+    validate(conf);
+    this.title = conf.title || "";
+    this.input = conf.input || "txt";
+    this.inputType = conf.inputType || "fs";
+    this.output = conf.output || "dist";
+    this.header = conf.header || "";
+    this.subheader = conf.subheader || "";
+    this.filename = conf.filename || "index.html";
+  }
 }
 
 function getBlankConf(confpath) {
-   if (existsSync(confpath)) {
-       return JSON.parse(readFileSync(confpath).toString());
-   }
-   throw new ConfigurationError("Configuration file does not exist");
+  if (existsSync(confpath)) {
+    return JSON.parse(readFileSync(confpath).toString());
+  }
+  throw new ConfigurationError("Configuration file does not exist");
 }
 
 function validate(config) {
-    if (isUndefined(config, "input")) {
-        throw new ConfigurationError("No input defined in website.json (input: 'dir where text files are')");
-    }
-    if (isUndefined(config, "output")) {
-        throw new ConfigurationError("No output defined in website.json (output: 'outputDir')");
-    }
-    if (!isUndefined(config, "inputType") && !(["fs", "git"].some((type) => type === config.inputType))) {
-        throw new ConfigurationError("Invalid input type. Choose from git|fs");
-    }
+  if (isUndefined(config, "input")) {
+    throw new ConfigurationError(
+      "No input defined in website.json (input: 'dir where text files are')",
+    );
+  }
+  if (isUndefined(config, "output")) {
+    throw new ConfigurationError(
+      "No output defined in website.json (output: 'outputDir')",
+    );
+  }
+  if (
+    !isUndefined(config, "inputType") &&
+    !["fs", "git"].some((type) => type === config.inputType)
+  ) {
+    throw new ConfigurationError("Invalid input type. Choose from git|fs");
+  }
 }
 
 function isUndefined(object: object, property: string) {
-    return !object.hasOwnProperty(property);
+  return !object.hasOwnProperty(property);
 }
