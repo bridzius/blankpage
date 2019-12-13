54eede659aaffe2490bec83979f499df7d0674db
Jurgis Bridzius <jurgis@bridzius.lt>
2019-12-13 22:18:55 +0200

    test: switched to jest for testing

--- src/composer.test.ts
+++ src/composer.test.ts
@@ -3,3 +3,8 @@
 // const origArgv = Array.from(process.argv);
 // Object.defineProperty(process, "argv", {value: ["", "", "hello.json"]});
 // Object.defineProperty(process, "argv", {value: origArgv});
+describe("composer.ts", () => {
+  it("should be dummy", () => {
+    expect(true).toBe(true);
+  });
+});
--- src/config.test.ts
+++ src/config.test.ts
@@ -1,49 +1,39 @@
-import { expect } from "chai";
 import * as fs from "fs";
-import "mocha";
-import * as sinon from "sinon";
 import { BlankpageConfig, IBlankConfig } from "./config";
 import { ConfigurationError } from "./config-error";
 let box;
 let config: IBlankConfig;
 describe("config.ts", () => {
-  before(() => {
-    box = sinon.createSandbox();
-  });
-
-  afterEach(() => {
-    box.restore();
-  });
   it("should throw an error if no path is provided to constructor", () => {
     expect(() => {
       config = new BlankpageConfig("");
-    }).to.throw(ConfigurationError);
+    }).toThrow(ConfigurationError);
   });
   it("should throw if no input is defined in website.json", () => {
-    box.stub(fs, "existsSync").returns(true);
-    box.stub(fs, "readFileSync").returns(`{
+    jest.spyOn(fs, "existsSync").mockReturnValue(true);
+    jest.spyOn(fs, "readFileSync").mockReturnValue(`{
             "title": "Website",
             "header": "My website",
             "output": "welcome"
         }`);
     expect(() => {
       config = new BlankpageConfig("hello.json");
-    }).to.throw(ConfigurationError);
+    }).toThrow(ConfigurationError);
   });
   it("should throw if no output is defined in website.json", () => {
-    box.stub(fs, "existsSync").returns(true);
-    box.stub(fs, "readFileSync").returns(`{
+    jest.spyOn(fs, "existsSync").mockReturnValue(true);
+    jest.spyOn(fs, "readFileSync").mockReturnValue(`{
             "title": "Website",
             "header": "My website",
             "input": "welcome"
             }`);
     expect(() => {
       config = new BlankpageConfig("hello.json");
-    }).to.throw(ConfigurationError);
+    }).toThrow(ConfigurationError);
   });
   it("should throw if inputType is random", () => {
-    box.stub(fs, "existsSync").returns(true);
-    box.stub(fs, "readFileSync").returns(`{
+    jest.spyOn(fs, "existsSync").mockReturnValue(true);
+    jest.spyOn(fs, "readFileSync").mockReturnValue(`{
             "title": "Website",
             "header": "My website",
             "input": "welcome",
@@ -52,11 +42,11 @@ describe("config.ts", () => {
         }`);
     expect(() => {
       config = new BlankpageConfig("hello.json");
-    }).to.throw(ConfigurationError);
+    }).toThrow(ConfigurationError);
   });
   it("should not throw if inputType is git", () => {
-    box.stub(fs, "existsSync").returns(true);
-    box.stub(fs, "readFileSync").returns(`{
+    jest.spyOn(fs, "existsSync").mockReturnValue(true);
+    jest.spyOn(fs, "readFileSync").mockReturnValue(`{
             "title": "Website",
             "header": "My website",
             "input": "welcome",
@@ -65,6 +55,6 @@ describe("config.ts", () => {
         }`);
     expect(() => {
       config = new BlankpageConfig("hello.json");
-    }).to.not.throw(ConfigurationError);
+    }).not.toThrow(ConfigurationError);
   });
 });
